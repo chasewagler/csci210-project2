4a5386c5a050219192c097610cb3336311bff5ed
chasewagler <chase_wagler@mines.edu>
2025-03-24 15:59:39 -0600

    First attempt

--- mkdirSplitpath.c
+++ mkdirSplitpath.c
@@ -1,4 +1,6 @@
 #include "types.h"
+#include <stdlib.h>
+#include <string.h>
 
 extern struct NODE* root;
 extern struct NODE* cwd;
@@ -6,24 +8,43 @@ extern struct NODE* cwd;
 //make directory
 void mkdir(char pathName[]){
 
-    // TO BE IMPLEMENTED
-    //
-    // YOUR CODE TO REPLACE THE PRINTF FUNCTION BELOW
+	struct NODE* newPath = (struct NODE*) malloc(sizeof(struct NODE));
+	newPath->childPtr = NULL;
+	newPath->fileType = 'd';
+	strncpy(newPath->name, 64, pathName);
+	newPath->parentPtr = cwd;
+	if (cwd->siblingPtr) {
+		newPath->siblingPtr = cwd->siblingPtr;
+	} else {
+		newPath->siblingPtr = NULL;
+	}
 
-    printf("TO BE IMPLEMENTED\n");
+	cwd->childPtr = newPath;
 
-    return;
+	return;
 }
 
 //handles tokenizing and absolute/relative pathing options
 struct NODE* splitPath(char* pathName, char* baseName, char* dirName){
-
-    // TO BE IMPLEMENTED
-    // NOTE THAT WITHOUT COMPLETING THIS FUNCTION CORRECTLY
-    // rm, rmdir, ls, cd, touch COMMANDS WILL NOT EXECUTE CORRECTLY
-    // SEE THE PROVIDED SOLUTION EXECUTABLE TO SEE THEIR EXPECTED BEHAVIOR
-
-    // YOUR CODE HERE
-    //
-    return NULL;
+	struct NODE* currentNode = cwd; // if it's a relative path, or root if it's an absolute path
+	char * currentDirName = strtok(pathName,"/"); // refer to the tokenization example at /u/pa/nb/tolgacan/210/spring25/strtokex.c
+	while (currentDirName != NULL) { // while there are still tokens
+		struct NODE* child = currentNode->childPtr;
+		while (child != NULL) {
+			if (strcmp(child->name,currentDirName)!=0) {
+				child = child->siblingPtr;
+			}
+			else break; // directory found among the children of currentNode
+		}
+		if (child == NULL) // not found
+		{
+			printf("Directory does not exist");
+			return NULL;
+		}
+		currentNode = child; // we traverse the tree to the next depth with this assignment
+		currentDirName = strtok(NULL,"/"); // now in the next iteration look for the next directory name
+	}
+		return NULL;
 }
+
+//compile with `gcc -g -o main main.c` in project folder
